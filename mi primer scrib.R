fd590e752320851464a4e0ebce8d9cfb3a7d6df9
krearga <krea.rga@gmail.com>
2019-04-13 11:11:06 -0400

    cree un nuevo repositorio

---
+++ mi primer scrib.R
@@ -0,0 +1,6 @@
+# instalar paquete ggplot2
+# install.packages("ggplot2")
+library(ggplot2) 
+
+source("primera funcion.R")
+resto(3,4,7)
---
+++ primera funcion.R
@@ -0,0 +1,29 @@
+funcionUno<- function(x,y){
+  ##  aqui va el desarrollo de la funcion
+  print("entro la suma")
+x+y
+ }
+
+# Realizar el desarrollo de velocidad de un cuerpo en relacion al tiempo
+# Funcion velocidad(double,double) -> double
+# Test velocidad(10,2) ->5
+
+velocidad <- function(distancia,tiempo){
+  print("la velocidad es")
+  print (distancia/tiempo)
+}
+velocidad(10,2)
+
+velocidad(9,4)
+
+#Realizar una funcion que calcule el resto de la multiplicacion de 2 numeros
+#Respecto a un tercero
+#Funcion resto: (int int int) -> int
+#Devuelve la evaluacion de a*b mod n
+#resto(3,4,7)-> 5
+
+resto <-function(x,y,z) {
+  (x*y)%%z
+}
+
+
